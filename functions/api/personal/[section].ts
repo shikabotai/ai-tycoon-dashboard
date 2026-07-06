d9b5e8d03daf48a296ab48335944e509d8ae1dcb
shikabotai <shikabotai@gmail.com>
2026-07-06 16:20:53 -0400

    Add dashboard personal projection function

---
+++ functions/api/personal/[section].ts
@@ -0,0 +1,22 @@
+import { getProjectedSection } from '../../../src/server/personalProjectionApi'
+
+export const onRequestGet: PagesFunction = async ({ params }) => {
+  const sectionParam = params.section
+  const section = Array.isArray(sectionParam) ? sectionParam[0] : sectionParam
+
+  if (!section) {
+    return new Response('Missing section', { status: 400 })
+  }
+
+  const data = getProjectedSection(section as Parameters<typeof getProjectedSection>[0])
+  if (!data) {
+    return new Response('Not found', { status: 404 })
+  }
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'cache-control': 'no-store',
+    },
+  })
+}
